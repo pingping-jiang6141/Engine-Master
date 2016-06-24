82da56bcffc623de296c5349d4556e12078a1c8b
蒋萍萍 <pingping.jiang@3g2win.com>
2016-06-24 14:52:55 +0800

    增加应用数据是否存放在沙箱目录开关及处理逻辑

--- Engine/src/org/zywx/wbpalmstar/base/BUtility.java
+++ Engine/src/org/zywx/wbpalmstar/base/BUtility.java
@@ -88,6 +88,7 @@ public class BUtility {
 
     public static boolean isDes = false;
     public static String g_desPath = "";
+    public static String widgetOneRootPath = "";
 
     // 缩放图片
     public static Bitmap imageScale(Bitmap bitmap, int dst_w, int dst_h) {
@@ -204,15 +205,32 @@ public class BUtility {
 
     }
 
+    /**
+     * @return 沙箱目录路径
+     */
+    public static String getSBoxRootPath(Context context) {
+        return context.getFilesDir().getAbsolutePath() + "/";
+    }
+
+    /**
+     * 得到widgetone的目录路径
+     * 
+     * @return ../widgetone/
+     */
+    public static String getWidgetOneRootPath() {
+        return widgetOneRootPath;
+    }
+
     // 初始化widget的文件夹
     public static void initWidgetOneFile(Context context, String appId) {
         String root = null;
         appId += "/";
-        if (sdCardIsWork()) {
+        if (!WDataManager.isWidgetOneSBox && sdCardIsWork()) {
             root = getSdCardRootPath();
         } else {
-            root = context.getFilesDir().getAbsolutePath() + "/";
+            root = getSBoxRootPath(context);
         }
+        widgetOneRootPath = root + F_BASE_WGT_PATH;
         String[] fileDir = {root + F_APP_PATH + appId, root + F_WIDGET_PATH,
                 root + F_APP_PATH + appId + F_APP_VIDEO,
                 root + F_APP_PATH + appId + F_APP_PHOTO,
